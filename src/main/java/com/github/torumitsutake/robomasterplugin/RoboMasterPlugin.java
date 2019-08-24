91c6d617565c2ef98be9c36c96c45ec906c36877
Toru Mitsutake <torumitsutake@gmail.com>
2019-08-24 19:57:12 +0900

    SnowBallDamageクラス作成

--- src/main/java/com/github/torumitsutake/robomasterplugin/RoboMasterPlugin.java
+++ src/main/java/com/github/torumitsutake/robomasterplugin/RoboMasterPlugin.java
@@ -1,5 +1,7 @@
 package com.github.torumitsutake.robomasterplugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RoboMasterPlugin extends JavaPlugin {
@@ -8,10 +10,21 @@ public final class RoboMasterPlugin extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+        //リスナー登録
+        getServer().getPluginManager().registerEvents(new SnowBallDamage(), this); //SnowBallDamageClass
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        if(cmd.getName().equalsIgnoreCase("cmd")){
+
+        }
+
+        return false;
+    }
+
 }
---
+++ src/main/java/com/github/torumitsutake/robomasterplugin/SnowBallDamage.java
@@ -0,0 +1,23 @@
+package com.github.torumitsutake.robomasterplugin;
+
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class SnowBallDamage implements Listener{
+
+	double SnowballDamage = 10.0;
+
+	@EventHandler
+	public void onEntityDamageEvent(EntityDamageByEntityEvent e){
+		if(e.getEntity() instanceof Player && e.getDamager() instanceof Snowball){
+			e.setDamage(SnowballDamage);
+		}
+
+
+	}
+
+
+}
